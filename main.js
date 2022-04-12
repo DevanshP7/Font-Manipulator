daaaa24e6c94b57fbccb57ea826bf71ad7cbf963
DevanshP7 <79050720+DevanshP7@users.noreply.github.com>
2022-04-12 16:52:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function setup(){
-    canvas = createCanvas(500, 500);
-    canvas.position(200, 200);
+    canvas = createCanvas(548, 411);
+    canvas.center();
 
     video = createCapture(VIDEO);
-    video.position(100,500);
-    video.size(320, 240);
+    video.position(100, 500);
+    video.size(320, 280);
 
     posenet = ml5.poseNet(video, model_loaded);
     posenet.on('pose', got_result);
@@ -17,5 +17,18 @@ function model_loaded(){
 function got_result(results){
     if(results.length > 0){
         console.log(results);
+
+        left_wrist_x = results[0].pose.leftWrist.x;
+        right_wrist_x = results[0].pose.rightWrist.x;
+
+        difference = floor(left_wrist_x - right_wrist_x);
+        console.log(`Left Wrist X = ${left_wrist_x} | Right Wrist X = ${right_wrist_x} | Difference = ${difference} |`);
     }
+}
+
+function draw(){
+    background('darkblue');
+    textSize(difference);
+    fill('white');
+    text('Devansh Pathak', 150, 202);
 }
